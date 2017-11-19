c7d7e8374085d9ff6be3e93196c7d8c4b91c81b4
Charles Brana <31863430+CharlesBrana@users.noreply.github.com>
2017-11-19 14:17:21 -0500

    not working

--- Tests/LightSensorTestProgram/src/ca/mcgill/ecse211/lab4/LocalizationType.java
+++
@@ -1,5 +0,0 @@
-package ca.mcgill.ecse211.lab4;
-
-public enum LocalizationType {
-	FALLINGEDGE, RISINGEDGE
-}
--- src/ca/mcgill/ecse211/lab5/LocalizationType.java
+++
@@ -1,6 +0,0 @@
-package ca.mcgill.ecse211.lab5;
-
-public enum LocalizationType {
-	// enumeration to distinguish which localization type to use for constructor
-	FALLINGEDGE, RISINGEDGE
-}
